db8254c575f3e701032c517afa7316a26b710ac2
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-27 09:00:31 +0200

    exercice 23 resolved

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -2,19 +2,24 @@ package com.campusdual.classroom;
 
 public class Doctor extends Person {
 
-	protected String specialization;
+    protected String specialization;
 
-	public Doctor(String name, String surname, String specialization) {
-		super(name, surname);
-		this.specialization = specialization;
-	}
+    public Doctor(String name, String surname, String specialization) {
+        super(name, surname);
+        this.specialization = specialization;
+    }
 
-	public String getSpecialization() {
-		return this.specialization;
-	}
+    public String getSpecialization() {
+        return this.specialization;
+    }
 
-	public void setSpecialization(String specialization) {
-		this.specialization = specialization;
-	}
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void getDetails() {
+        System.out.println("Doctor " + name + " " + surname + ", especialista en " + specialization.toLowerCase());
+    }
 
 }
--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -3,18 +3,34 @@ package com.campusdual.classroom;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
+        Person person = new Person("John", "Smith");
+        Person teacher = new Teacher("María", "Montessori", "Educación");
+        Person police = new PoliceOfficer("Jake", "Peralta", "B-99");
+        Person doctor = new Doctor("Gregory", "House", "Nefrología e infectología");
 
+        Map<String, Person> hashMap = new HashMap<>();
+        hashMap.put("person", person);
+        hashMap.put("teacher", teacher);
+        hashMap.put("police", police);
+        hashMap.put("doctor", doctor);
+
+        return hashMap;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        Person police2 = new PoliceOfficer("Charles", "Boyle", "B-99");
+        map.put(key, value);
+        return value;
     }
 
     public static void printMapValues(Map<String, Person> map) {
-
+        for (Map.Entry<String, Person> entry : map.entrySet()) {
+            entry.getValue().getDetails();
+        }
     }
-    
 }
